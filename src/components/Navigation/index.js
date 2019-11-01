f9eea554b25a60caabc589c3d8af55415dfdca82
kigningcole <43896019+kigningcole@users.noreply.github.com>
2019-11-01 01:06:07 +0100

    initial commit

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,57 @@
+import React, { useContext, useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+
+import StoreContext from '~/context/StoreContext'
+import { 
+	Wrapper,
+	Container,
+	CartCounter,
+	MenuLink
+} from './styles'
+
+const countQuantity = lineItems => {
+	let quantity = 0
+
+	lineItems.forEach(item => {
+		quantity = quantity + item.quantity
+	});
+
+	return quantity
+}
+
+const Navigation = ({ siteTitle }) => {
+	const { store: {checkout} } = useContext(StoreContext)
+	const [quantity, setQuantity] = useState(countQuantity(checkout ? checkout.lineItems : []))
+
+	useEffect(() => {
+		setQuantity(countQuantity(checkout ? checkout.lineItems : []));
+	}, [checkout]);
+
+	return(
+		<Wrapper>
+			<Container>
+				<MenuLink to='/'>
+					{siteTitle}
+				</MenuLink>
+				<MenuLink to='/cart'>
+					{quantity !== 0 &&
+						<CartCounter>
+							{quantity}
+						</CartCounter>
+					}
+					Cart 🛍
+				</MenuLink>
+			</Container>
+		</Wrapper>
+	)
+}
+
+Navigation.propTypes = {
+	siteTitle: PropTypes.string,
+}
+
+Navigation.defaultProps = {
+	siteTitle: ``,
+}
+
+export default Navigation
